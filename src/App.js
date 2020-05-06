1524486cdf6207e539ebac7c2e57c86cac528a28
Stephen O'Connor <stephen.oconnor@collectivworks.com>
2020-05-06 09:25:34 +0100

    Added basic tests.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Test = () => <div>Testing</div>;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1>React</h1>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<ul className="test">
+					<li>One</li>
+					<li>Two</li>
+					<li>Three</li>
+				</ul>
+			</header>
+			<Test />
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Enzyme adapter needs to be setup for React 16.
+configure({ adapter: new Adapter() });
+
+// Wrap tests in a describe block.
+describe("<App />", () => {
+	// shallow is used to test individual components, doesn't care about child components.
+	const wrapper = shallow(<App />, {
+		context: {},
+		disableLifecycleMethods: true,
+	});
+
+	// Individual test(s).
+	it("should render App", () => {
+		console.log(wrapper.debug());
+	});
+
+	// Find a DOM element.
+	it("should contain 1 a element", () => {
+		expect(wrapper.find("a").length).toBe(1);
+	});
+
+	// Check if a DOM element exists.
+	it("a element should have a class of App-link", () => {
+		expect(wrapper.find(".App-link").exists()).toBe(true);
+	});
+
+	// Scope test to parent --> child hierarchy.
+	it("should contain 3 list items", () => {
+		expect(wrapper.find("ul").children().length).toBe(3);
+	});
+
+	// Check for a class name.
+	it("should have a class of test", () => {
+		expect(wrapper.find("ul").hasClass("test")).toBe(true);
+	});
+
+	// Check for text.
+	it("should have an h1 with specified text", () => {
+		expect(wrapper.find("h1").text()).toBe("React");
+	});
 });
